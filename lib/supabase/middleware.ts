3cc6a61ff3faf6bb3d132a98665e52ba2ab9c46e
mcthequeen <jeremrichard20@gmail.com>
2025-01-28 21:43:26 +0100

    corrected middleware

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -44,20 +44,10 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/signin") &&
     !request.nextUrl.pathname.startsWith("/signup")
   ) {
-    // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/signin";
     return NextResponse.redirect(url);
   }
 
-  if (!user && request.nextUrl.pathname.startsWith("/")) {
-    if (!request.nextUrl.pathname.startsWith("/signin")) {
-      // no user, potentially respond by redirecting the user to the login page
-      const url = request.nextUrl.clone();
-      url.pathname = "/signin";
-      return NextResponse.redirect(url);
-    }
-  }
-
-  return supabaseResponse;
+  return NextResponse.next();
 }
